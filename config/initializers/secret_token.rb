d204b2622c8c64e2a75edf64fd5abb0d218689cc
Rudy <rudy@oxynum.fr>
2012-08-07 01:09:34 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResidenceGoldwyn::Application.config.secret_token = '9b429d62f2e2ba559bb6095003ce27bad35c6b2dbe7aaec7ed5faa34764c0c4c2df9d9063bedf7c5520853e6ba633b6e17521aac2e407fd27f9ce05cb73c3bca'
