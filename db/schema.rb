04755a9b7987273481ed303c1ae4c94ce3159ee4
Rudy <rudy@oxynum.fr>
2012-08-25 14:27:12 +0200

    change supersized images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120821223335) do
+ActiveRecord::Schema.define(:version => 20120819223004) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -47,20 +47,17 @@ ActiveRecord::Schema.define(:version => 20120821223335) do
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "rooms", :force => true do |t|
-    t.string   "name"
+    t.string   "name",                                     :null => false
     t.text     "description"
     t.string   "dimension"
-    t.datetime "created_at",                                  :null => false
-    t.datetime "updated_at",                                  :null => false
-    t.string   "status",             :default => "avalaible"
-    t.integer  "number_of_rooms",    :default => 1
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
+    t.string   "status",          :default => "avalaible"
+    t.integer  "number_of_rooms", :default => 1
     t.string   "sleeping"
+    t.string   "image_url"
     t.integer  "day_price"
     t.integer  "week_price"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "rooms_stays", :id => false, :force => true do |t|
@@ -81,9 +78,9 @@ ActiveRecord::Schema.define(:version => 20120821223335) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "stays", :force => true do |t|
-    t.datetime "arrival_date"
+    t.datetime "arrival_date",                          :null => false
     t.datetime "departure_date"
-    t.integer  "user_id"
+    t.integer  "user_id",                               :null => false
     t.datetime "created_at",                            :null => false
     t.datetime "updated_at",                            :null => false
     t.boolean  "paid",               :default => false
