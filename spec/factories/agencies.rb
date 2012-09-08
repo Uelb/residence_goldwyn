a851b71eaddb69454949dbe578baf78b1b1b51b4
Thomas <thomas@oxynum.fr>
2012-09-09 08:56:22 +0900

    fix

---
+++ spec/factories/agencies.rb
@@ -0,0 +1,12 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :agency do
+    name "MyString"
+    password "MyString"
+    client_first_name "MyString"
+    clien_last_name "MyString"
+    email "MyString"
+    tva_number "MyString"
+  end
+end
