6053d100e69947eb3cfe3832b24eeb09b356d286
Thomas <thomas@oxynum.fr>
2012-09-21 19:04:20 +0900

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,11 @@ ResidenceGoldwyn::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address              => "ks385986.kimsufi.com",
+  :domain               => "residencegoldwyn.fr",
+  :enable_starttls_auto => true
+}
 
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
