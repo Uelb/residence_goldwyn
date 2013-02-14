1eb8eec983bfd259fea475c0e129e6186f94b543
Rudy <dathims@gmail.com>
2013-02-14 02:05:32 +0100

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,7 @@ AdminUser.create(:email => "thomas@oxynum.fr", :password => "azertyuiop", :passw
 AdminUser.create(:email => "rudy@oxynum.fr", :password => "poiuytreza", :password_confirmation => "poiuytreza")
 AdminUser.create(:email => "samy@oxynum.fr", :password => "bleuciel", :password_confirmation => "bleuciel")
 
-Room.create(:name => "Duo Room", :sleeping => "2 lits simples ou 1 lit double", :number_of_rooms => 2, :dimension => "35 m2", :description => "Chambre supérieure, idéale pour un repos bien mérité")
-Room.create(:name => "Duo Room", :sleeping => "2 lits simples ou 1 lit double", :week_price => "1500", :number_of_rooms => 2, :dimension => "35 m2", :description => "Chambre supérieure, idéale pour un repos bien mérité")
+Room.create(name: "La petite canette", description: "Venez passer un moment de plaisir dans notre magnifique suite au bord de la méditéranée. Un bonheur pour tout la famille", dimension: "50m2 de suface", number_of_rooms: 1, sleeping: "Lit double queen size 180x210", week_price: 660, day_price: 95)
+Room.create(name: "La petite canette 2", description: "Venez passer un moment de plaisir dans notre magnifique suite au bord de la méditéranée. Un bonheur pour tout la famille", dimension: "50m2 de suface", number_of_rooms: 1, sleeping: "Lit double queen size 180x210", week_price: 660, day_price: 95)
+Room.create(name: "La petite canette 3", description: "Venez passer un moment de plaisir dans notre magnifique suite au bord de la méditéranée. Un bonheur pour tout la famille", dimension: "50m2 de suface", number_of_rooms: 1, sleeping: "Lit double queen size 180x210", week_price: 660, day_price: 95)
+Room.create(name: "La petite canette 4", description: "Venez passer un moment de plaisir dans notre magnifique suite au bord de la méditéranée. Un bonheur pour tout la famille", dimension: "50m2 de suface", number_of_rooms: 1, sleeping: "Lit double queen size 180x210", week_price: 660, day_price: 95)
