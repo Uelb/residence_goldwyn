0c8644fe8fa307a75ceb57a786e09e0c0eddab6d
Thomas THIMOTHEE <thomas@oxynum.fr>
2013-02-20 20:56:08 +0100

    fix image upload problem

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,7 +6,7 @@ class Room < ActiveRecord::Base
   DEFAULT_URL= "missing/original/missing.png"
   
   attr_accessible :description, :dimension, :name, :status, :sleeping, :number_of_rooms, :day_price, :week_price 
-  attr_accessible :image
+  attr_accessible :image, :image_2, :image_3, :image_4, :image_5
   has_attached_file :image, :default_url => DEFAULT_URL,  :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_attached_file :image_2, :default_url => DEFAULT_URL,  :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_attached_file :image_3, :default_url => DEFAULT_URL,  :styles => { :medium => "300x300>", :thumb => "100x100>" }
